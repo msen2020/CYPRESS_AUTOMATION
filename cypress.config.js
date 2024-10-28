1970f45d85880191da44539e07778b435b7bd2db
msen2020 <msen2020@hotmail.com>
2024-10-28 16:57:21 +0100

    Registration.cy.js has been updated

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.family === 'chromium' && browser.name !== 'electron') {
+          launchOptions.args.push('--ignore-certificate-errors');
+        }
+        return launchOptions;
+      });
     },
   },
 });
--- cypress/e2e/firstTest.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Launch the Application', () => {
-  it('Launch the test application', () => {
-    cy.visit('https://www.saucedemo.com')
-    cy.
-  })
-  
-})
-
